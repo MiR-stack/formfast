bed9132f4383ad0ec2c042b0d6f356eed1eb4635
habibmir808 <habibmir808@gmail.com>
2024-03-27 22:30:30 +0600

    reamove unneccesery code

--- lib/form/field.d.ts
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { newElementTypes } from "../types";
-declare const Field: React.FC<newElementTypes>;
-export default Field;
--- lib/form/form.d.ts
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { formPropsType } from "../types";
-declare const Form: React.FC<formPropsType>;
-export default Form;
